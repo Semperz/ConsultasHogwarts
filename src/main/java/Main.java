3a8608aefa10b77ea3a7d7d7fc178daf95b4cb1b
a23sergiopg <serprigar@gmail.com>
2024-12-16 13:39:46 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import model.HousePoint;
+import model.Person;
+
+
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        //studentsByProfesor(99);
+
+        bestStudent();
+    }
+
+
+    public static void studentsByProfesor (Integer idProfesor){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+
+        Query qr= em.createQuery("select p from Enrollment e JOIN e.personEnrollment p WHERE e.courseEnrollment.teacher.id = :idProfesor");
+        qr.setParameter("idProfesor", idProfesor);
+        List<Person> nombresAlumnos = qr.getResultList();
+        int numAlumnos = 0;
+        try {
+            for (Person p : nombresAlumnos) {
+                System.out.println(p.getFirstName());
+                numAlumnos +=  1;
+            }
+            System.out.println("Numero de alumnos: " + numAlumnos);
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+
+
+    public static void bestStudent(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+
+        Query qr= em.createQuery("select hp.receiver from HousePoint hp group by hp.receiver order by sum(hp.points) desc LIMIT 1");
+        Object bestStudent = qr.getSingleResult();
+        try {
+            System.out.println(bestStudent.toString());
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
